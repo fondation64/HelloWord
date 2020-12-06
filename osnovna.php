f5c745bf2e21e3d8de37d9d9086b059a16f4aa96
fondation64 <75547407+fondation64@users.noreply.github.com>
2020-12-06 13:13:40 +0200

    osnovna.php
    new effect

---
+++ osnovna.php
@@ -0,0 +1,37 @@
+<style>
+.hover01 figure img {
+	-webkit-transform: scale(1);
+	transform: scale(1);
+	-webkit-transition: .3s ease-in-out;
+	transition: .3s ease-in-out;
+}
+.hover01 figure:hover img {
+	-webkit-transform: scale(1.3);
+	transform: scale(1.3);
+}
+</style>
+<html>
+<head>
+<title>Хранителни стоки</title>
+<link rel="stylesheet" href="stil.css" />
+
+</head>
+<body>
+<h2 id="demo01">1. Zoom In #1</h2>
+<div class="hover01 column">
+  <div>
+    <figure><img src="https://picsum.photos/300/200?image=244" /></figure>
+    <span>Hover</span>
+  </div>
+  <div>
+    <figure><img src="https://picsum.photos/300/200?image=1024" /></figure>
+    <span>Hover</span>
+  </div>
+  <div>
+    <figure><img src="https://picsum.photos/300/200?image=611" /></figure>
+    <span>Hover</span>
+  </div>
+</div>
+
+</body>
+</html>
